d1ba9b47e1bc039ef91398faf84a2418ae09bb6f
Biyenga <aimerance.biyenga@kadea.local>
2023-10-25 21:01:35 +0100

    La valeur de x

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 //Declare a variable firstName and initialize it with the value 'Lata'
 const firstName = 'Lata';
 //Which value does x have after execution of the following code?
-let x = 'Geeta';
+//let x = 'Geeta';
 //Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'
 let flower = 'rose';
 let tree = 'maple';
+//Which value does x have after execution of the following code?
+let x = 'Tic';
+x = 'Tac';
+x = 'Toe';

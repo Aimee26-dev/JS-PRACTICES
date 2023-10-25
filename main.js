5249fc934c7246e1815c1890a2b95be01d093009
Biyenga <aimerance.biyenga@kadea.local>
2023-10-25 20:11:04 +0100

    Declare two variable

--- main.js
+++ main.js
@@ -2,3 +2,6 @@
 const firstName = 'Lata';
 //Which value does x have after execution of the following code?
 let x = 'Geeta';
+//Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'
+let flower = 'rose';
+let tree = 'maple';

a3691f9270ecde1f9d9f2f1936f41aad66a4c053
Biyenga <aimerance.biyenga@kadea.local>
2023-10-26 19:41:42 +0100

    Definir une fonction hello

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 //Declare a variable firstName and initialize it with the value 'Lata'
 const firstName = 'Lata';
+
 //Which value does x have after execution of the following code?
 //let x = 'Geeta';
+
 //Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'
 let flower = 'rose';
 let tree = 'maple';
+
 //Which value does x have after execution of the following code?
 //let x = 'Tic';
 //x = 'Tac';
 //x = 'Toe';
-//Which value does x have after execution of the following code?  
+
+/*Which value does x have after execution of the following code?  
 let x = 'Laurel';
 let y = 'Hardy';
 let z = 'y';
-x = z;
+x = z;*/
+
 //Define a function hello that returns 'Hello world
 function hello(){
     return "Hello world!";
   }
+  
   //Define two functions. The first function a should return 'Hello a!' and the second function b should return 'Hello b!'
   function a(){
     return 'Hello a!'
@@ -31,3 +37,9 @@ function greet(){
   return 'Haydo!';
 }
 const salutation = greet();
+
+//Definir une fonction hello
+function hello(){
+  return 'Hi';
+}
+let x = hello();

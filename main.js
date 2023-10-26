6b203bdfd72703260bd35960169d601330d59826
Biyenga <aimerance.biyenga@kadea.local>
2023-10-26 21:00:13 +0100

    Definir deux fonction qui retourne deux parametre

--- main.js
+++ main.js
@@ -1,45 +1,68 @@
 //Declare a variable firstName and initialize it with the value 'Lata'
 const firstName = 'Lata';
+console.log(firstName);
 
 //Which value does x have after execution of the following code?
 //let x = 'Geeta';
+console.log(x);
 
 //Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'
 let flower = 'rose';
+console.log(flower);
 let tree = 'maple';
+console.log(tree);
 
 //Which value does x have after execution of the following code?
 //let x = 'Tic';
 //x = 'Tac';
 //x = 'Toe';
+console.log(x);
 
 /*Which value does x have after execution of the following code?  
 let x = 'Laurel';
 let y = 'Hardy';
 let z = 'y';
 x = z;*/
+console.log(x);
 
 //Define a function hello that returns 'Hello world
 function hello(){
     return "Hello world!";
   }
-  
+console.log(hello());
+
   //Define two functions. The first function a should return 'Hello a!' and the second function b should return 'Hello b!'
   function a(){
     return 'Hello a!'
   }
+  console.log(a());
+
   function b(){
     return 'Hello b!'
   }
+console.log(b());
 
 //Define a function greet returning the value  'Haydo!'   Declare a variable salutation Call the function greet and assign the result of the call to the variable salutation.
 function greet(){
   return 'Haydo!';
 }
 const salutation = greet();
+console.log(salutation);
 
 //Definir une fonction hello
 function hello(){
   return 'Hi';
 }
 let x = hello();
+console.log(x);
+
+//Write a function echo that also returns the passed parameter. echo('Greta') should return 'Greta' and echo('CO2') should return 'CO2
+function echo(){
+  return 'Greta';
+}
+console.log(echo());
+
+function echo(){
+  return 'CO2';
+}
+console.log(echo());
